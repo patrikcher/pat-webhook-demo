7f8c523fb059f0b94a1358d358b0b5d1264cca39
patrikcher <patrickcher@gmail.com>
2018-03-21 22:24:18 +0800

    added new attribute for display purpose
    added new attribute for display purpose

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ if($method == 'POST'){
 	switch ($text) {
 		case 'hi':
 			$speech = 'Hi, Nice to meet you';
+			$display = $speech;
 			break;
 			
 		case 'book review':
@@ -22,18 +23,22 @@ if($method == 'POST'){
 			switch ($num) {
 				case 1:
 					$speech = '<speak><audio src="https://pat-webhook-demo.herokuapp.com/rsc/BlackPanther.mp3"><desc>Black Panther: Who is Black Panter?</desc>did not get your audio file</audio></speak>';
+					$display = 'Black Panther: Who is Black Panther?';
 					break;
 					
 				case 2:
 					$speech = '<speak><audio src="https://pat-webhook-demo.herokuapp.com/rsc/From%20the%20Belly%20of%20the%20Cat.mp3"><desc>From the Belly of the Cat</desc>did not get your audio file</audio></speak>';
+					$display = 'From the Belly of the Cat';
 					break;
 					
 				case 3:
 					$speech = '<speak><audio src="https://pat-webhook-demo.herokuapp.com/rsc/ArivaalJeevitham_F2.mp3"><desc>Arivval Jeevitham</desc>did not get your audio file</audio></speak>';
+					$display = 'Arivval Jeevitham';
 					break;
 				
 				default:
 					$speech = 'Something went wrong. I did not manage to get your book review.';
+					$display = $speech;
 					break;
 			}
 			
@@ -41,20 +46,23 @@ if($method == 'POST'){
 
 		case 'bye':
 			$speech = 'Bye, good night';
+			$display = $speech;
 			break;
 
 		case 'anything':
 			$speech = 'Yes, you can type anything here.';
+			$display = $speech;
 			break;
 		
 		default:
 			$speech = 'Sorry, I didnt get that. Please ask me something else.';
+			$display = $speech;
 			break;
 	}
 
 	$response = new \stdClass();
 	$response->speech = $speech;
-	$response->displayText = $speech;
+	$response->displayText = $display;
 	$response->source = "webhook";
 	echo json_encode($response);
 }
