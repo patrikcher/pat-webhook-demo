c3c8abca7088b40a21c289b0094a454ae8ffe773
patrikcher <patrickcher@gmail.com>
2018-03-22 13:29:20 +0800

    simplified logic for retrieving book review

--- index.php
+++ index.php
@@ -23,33 +23,9 @@ if($method == 'POST'){
 		case ($text == 'book review' || $text == 'read me a book review' || $text == 'read a book review' || strpos($text, 'sure') !== false || strpos($text, 'yes') !== false || strpos($text, 'sure') !== false):
 			$num = rand(1, 3);
 			
-			switch ($num) {
-				case 1:
-					$speech = '<speak><audio src="' . $bookReviewsJson[$num-1]['filepath']  . 
+			$speech = '<speak><audio src="' . $bookReviewsJson[$num-1]['filepath']  . 
 							'"><desc>' . $bookReviewsJson[$num-1]['title'] . '</desc>I did not manage to get your book review.</audio></speak>';
-					$display = 'Now reading book review for ' . $bookReviewsJson[$num-1]['title'] . '. URL is . ' . $bookReviewsJson[$num-1]['filepath'] . 
-							' Next review?';
-					break;
-					
-				case 2:
-					$speech = '<speak><audio src="https://pat-webhook-demo.herokuapp.com/rsc/From%20the%20Belly%20of%20the%20Cat.mp3">' .
-							'<desc>From the Belly of the Cat</desc>I did not manage to get your book review.</audio></speak>';
-					$display = 'Now reading book review for ' . $bookReviewsJson[$num-1]['title'] . '. URL is . ' . $bookReviewsJson[$num-1]['filepath'] . 
-							' Next review?';
-					break;
-					
-				case 3:
-					$speech = '<speak><audio src="https://pat-webhook-demo.herokuapp.com/rsc/ArivaalJeevitham_F2.mp3">' .
-							'<desc>Arivval Jeevitham</desc>I did not manage to get your book review.</audio></speak>';
-					$display = 'Now reading book review for ' . $bookReviewsJson[$num-1]['title'] . '. URL is . ' . $bookReviewsJson[$num-1]['filepath'] . 
-							' Next review?';
-					break;
-				
-				default:
-					$speech = 'I did not manage to get your book review.';
-					$display = $speech;
-					break;
-			}
+					$display = 'Now reading book review for ' . $bookReviewsJson[$num-1]['title'] . '. Next review?';
 			
 			break;
 
