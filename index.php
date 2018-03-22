7016f9073ce6cae083319c40ec2ef010d0b6d097
patrikcher <patrickcher@gmail.com>
2018-03-22 10:28:07 +0800

    changed display text and added exit conditions
    changed display text and added exit conditions

--- index.php
+++ index.php
@@ -17,36 +17,36 @@ if($method == 'POST'){
 			
 			break;
 			
-		case ($text == 'book review' || $text == 'read me a book review' || $text == 'read a book review' || $text == 'next' || $text == 'yes'):
+		case ($text == 'book review' || $text == 'read me a book review' || $text == 'read a book review' || $text == 'next' || $text == 'yes' || $text == 'sure'):
 			$num = rand(1, 3);
 			//$speech = 'Here is a book review that may interest you.';
 			
 			switch ($num) {
 				case 1:
 					$speech = '<speak><audio src="https://pat-webhook-demo.herokuapp.com/rsc/BlackPanther.mp3"><desc>Black Panther: Who is Black Panter?</desc>did not get your audio file</audio></speak>';
-					$display = 'Current review is about "Black Panther: Who is Black Panther?". Next review?';
+					$display = 'Now reading book review for "Black Panther: Who is Black Panther?". Next review?';
 					break;
 					
 				case 2:
 					$speech = '<speak><audio src="https://pat-webhook-demo.herokuapp.com/rsc/From%20the%20Belly%20of%20the%20Cat.mp3"><desc>From the Belly of the Cat</desc>did not get your audio file</audio></speak>';
-					$display = 'Current review is about "From the Belly of the Cat". Next review?';
+					$display = 'Now reading book review for "From the Belly of the Cat". Next review?';
 					break;
 					
 				case 3:
 					$speech = '<speak><audio src="https://pat-webhook-demo.herokuapp.com/rsc/ArivaalJeevitham_F2.mp3"><desc>Arivval Jeevitham</desc>did not get your audio file</audio></speak>';
-					$display = 'Current review is about "Arivval Jeevitham". Next review?';
+					$display = 'Now reading book review for "Arivval Jeevitham". Next review?';
 					break;
 				
 				default:
-					$speech = 'Something went wrong. I did not manage to get your book review.';
+					$speech = 'I did not manage to get your book review.';
 					$display = $speech;
 					break;
 			}
 			
 			break;
 
-		case 'bye':
-			$speech = 'Bye, good night';
+		case ($text == 'bye' || $text == 'no'):
+			$speech = 'Goodbye, come again soon.';
 			$display = $speech;
 			
 			break;
