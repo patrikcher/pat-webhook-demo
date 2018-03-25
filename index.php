62e33977472ebb0767f32d603d06bb4e8634d222
patrikcher <patrickcher@gmail.com>
2018-03-25 23:29:24 +0800

    update index.php
    remove review debugging message

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ if($method == 'POST'){
 			$review = $bookReviewsJson[$num]['review'];
 			
 			$speech = '<speak><audio src="' . $filepath  . '"><desc>' . $title . '</desc>I did not manage to get your book review.</audio>Would you like me to read another review?</speak>';
-			$display = 'Now reading book review for ' . $title . '. Would you like me to read another review?' . $review;
+			$display = 'Now reading book review for ' . $title . '. Would you like me to read another review?';
 			
 			break;
 
