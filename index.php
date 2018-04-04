3a83a91fdf4b2af9cc46c6bb488311ab308125c6
patrikcher <patrickcher@gmail.com>
2018-04-04 09:34:31 +0800

    update index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ if($method == 'POST'){
 			$speech = '<speak>' . $title . ' written by ' . $author . '<break time="2s"/>' . 
 				'<audio src="' . $filepath  . '"><desc>' . $title . '</desc>I did not manage to get your book review.</audio>' . 
 				'Would you like me to read another review?</speak>';
-			$display = 'Now reading book review for ' . $title . '. Would you like me to read another review?';
+			$display = 'Now reading book review for ' . $title . '. Would you like me to read another review?' . ' count is ' . count($bookReviewsJson)-1;
 			
 			unset($bookReviewsJson[$num]);
 			
