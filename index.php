bfda446f781db1cb1450eaad790dfcff2ae9d2c1
patrikcher <patrickcher@gmail.com>
2018-04-04 15:39:39 +0800

    update index.php

--- index.php
+++ index.php
@@ -39,20 +39,6 @@ if($method == 'POST'){
 			$display = 'Now reading book review for ' . $title . '. Would you like me to read another review?';
 			
 			break;
-			
-		case ($text == 'repeat' || $text == 'again'):
-			if ($title != '') {
-				$speech = '<speak>' . $title . ' written by ' . $author . '<break time="2s"/>' . 
-					'<audio src="' . $filepath  . '"><desc>' . $title . '</desc>I did not manage to get your book review.</audio>' . 
-					'Would you like me to read another review?</speak>';
-				$display = 'Now reading book review for ' . $title . '. Would you like me to read another review?';
-			}
-			else {
-				$speech = '';
-				$display = 'Nothing to repeat.';
-			}
-			
-			break;
 
 		case ($text == 'bye' || $text == 'no' || $text == 'pass'):
 			$speech = 'Goodbye, come again soon.';
