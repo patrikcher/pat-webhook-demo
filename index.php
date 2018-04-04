d11bc2136bb0b5d60066f76406e5890e68568880
patrikcher <patrickcher@gmail.com>
2018-04-04 15:57:40 +0800

    update index.php

--- index.php
+++ index.php
@@ -40,15 +40,6 @@ if($method == 'POST'){
 			
 			break;
 		
-		case ($text == 'repeat' || $text == 'again'):
-			$speech = '<speak>' . $title . ' written by ' . $author . '<break time="2s"/>' . 
-				'<audio src="' . $filepath  . '"><desc>' . $title . '</desc>I did not manage to get your book review.</audio>' . 
-				'Would you like me to read another review?</speak>';
-			$display = 'Now reading book review for ' . $title . '. Would you like me to read another review?';
-			
-			break;
-			
-
 		case ($text == 'bye' || $text == 'no' || $text == 'pass'):
 			$speech = 'Goodbye, come again soon.';
 			$display = $speech;
